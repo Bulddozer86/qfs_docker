96271db9e610dbb9fe5c2b9082c9bcb24c0ff5f8
Victor <bulddozer86@gmail.com>
2017-06-06 23:55:19 +0300

    added autobuild docker images if it needed

--- run.sh
+++ run.sh
@@ -6,20 +6,19 @@ IMAGES=(${!ARRAY_CUSTOM_DOCKER_IMAGES[@]})
 
 echo ">> Start building of docker images....";
 
-for (( I=0; $I < ${#ARRAY_CUSTOM_DOCKER_IMAGES[@]}; I+=1 )); do
-    IMAGE=${IMAGES[$I]};
+for (( i=0; $i < ${#ARRAY_CUSTOM_DOCKER_IMAGES[@]}; i+=1 )); do
+    IMAGE=${IMAGES[$i]};
 
-    docker images | grep ${IMAGE}
+    grep_result=$(docker images | grep ${IMAGE})
     greprc=$?
 
     if [[ ! $greprc -eq 0 ]]; then
-      echo "Need building image ${IMAGE}"
+        echo "Start building image container ${IMAGE}"
+        cd ${ARRAY_CUSTOM_DOCKER_IMAGES[$IMAGE]}
+        docker build -t=${IMAGE} .
+        cd ..
     else
-      echo "Images ${IMAGE} installed already!"
-      cd ${ARRAY_CUSTOM_DOCKER_IMAGES[$IMAGE]}
-#      TODO:: will add docker build there
-      cd ..
-
+        echo "Images ${IMAGE} builded already!"
     fi
 #    echo $IMAGE --- ${ARRAY_CUSTOM_DOCKER_IMAGES[$IMAGE]};
 done
